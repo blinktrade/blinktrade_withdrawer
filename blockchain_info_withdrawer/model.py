6230b72fef045f111f459ceb4b2bf090cc5291e9
rodrigo <rodrigo@temporadafora.com>
2014-10-01 06:38:24 -0400

    importing jsonw

--- blockchain_info_withdrawer/model.py
+++ blockchain_info_withdrawer/model.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import  relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 import datetime
+import json
 
 Base = declarative_base()
 class Withdraw(Base):
